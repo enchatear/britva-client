f44c46ba8050e186238bf9487f601711b6688851
zmontee@gmail.com <zmontee@gmail.com>
2025-09-29 12:48:29 +0300

    strapi default url

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 export const config = {
   strapi: {
-    baseUrl: process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
+    baseUrl:
+      process.env.NEXT_PUBLIC_STRAPI_URL || 'http://admin.britvairpin.com.ua',
     apiToken: process.env.STRAPI_API_TOKEN,
     previewSecret: process.env.STRAPI_PREVIEW_SECRET,
     revalidateToken: process.env.REVALIDATE_TOKEN,
